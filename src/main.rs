2ac419e12a23d14148ed9297b6aabbc459610bea
jonathanrundjan <jonathan.yitskhaq@ui.ac.id>
2026-05-12 09:42:15 +0700

    Initial subscriber code

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, HandleError, MessageHandler};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        println!(
+            "In Jonathan's Computer 2406435231. Message received: {:?}",
+            message
+        );
+        Ok(())
+    }
+}
+
+fn main() {
+    let listener =
+        CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned())
+            .unwrap();
+
+    _ = listener.listen(
+        "user_created".to_owned(),
+        UserCreatedHandler {},
+        crosstown_bus::QueueProperties {
+            auto_delete: false,
+            durable: false,
+            use_dead_letter: true,
+        },
+    );
+
+    loop {}
+}
